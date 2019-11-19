8ff109f7cb377f4c0d29b9713ef787b8c1a8e1a0
JanetteDevCode <30553904+JanetteDevCode@users.noreply.github.com>
2019-11-19 12:36:46 -0800

    Complete helper functions and unit tests step 2

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,15 @@ const dummy = (blogs) => {
   return 1;
 };
 
+const totalLikes = (blogs) => {
+  const initialValue = 0;
+
+  return blogs.reduce((accumulator, currentValue) => {
+    return accumulator + currentValue.likes;
+  }, initialValue);
+};
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 };
